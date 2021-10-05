04d6597dd3ba051e6f83a6d977bf6adb31eb3cb9
Chris Freas <code@packetbusters.net>
2021-10-05 17:20:06 -0400

    Improve error handling. Restructure HTTP response status into an enum.

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,82 @@
 extern crate term;
+use anyhow::{Context, Result};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use url::{ParseError, Url};
+use url::Url;
 
 type Urls = Vec<String>;
 type CheckedUrls = HashMap<String, u16>;
 
-async fn _make_urls(fname: &str) -> Result<Urls, ParseError> {
+enum Status {
+    Success(String),
+    Error(String),
+    Warning(String),
+}
+
+impl Status {
+    fn new(status_code: u16, msg: &str) -> Self {
+        match status_code {
+            200..=299 => {
+                let message = format!("{} - {}", status_code, msg);
+                Status::Success(message)
+            }
+            400..=499 => {
+                let message = format!("{} - {}", status_code, msg);
+                Status::Warning(message)
+            }
+            _ => {
+                let message = format!("{} - {}", status_code, msg);
+                Status::Error(message)
+            }
+        }
+    }
+    fn print(&self) {
+        let mut t = term::stdout().unwrap();
+        match self {
+            Status::Success(msg) => {
+                t.fg(term::color::GREEN).unwrap();
+                write!(t, "◉ ").unwrap();
+                t.fg(term::color::WHITE).unwrap();
+                writeln!(t, "{}", msg).unwrap();
+            }
+            Status::Error(msg) => {
+                t.fg(term::color::RED).unwrap();
+                write!(t, "◉ ").unwrap();
+                t.fg(term::color::WHITE).unwrap();
+                writeln!(t, "{}", msg).unwrap();
+            }
+            Status::Warning(msg) => {
+                t.fg(term::color::YELLOW).unwrap();
+                write!(t, "◉ ").unwrap();
+                t.fg(term::color::WHITE).unwrap();
+                writeln!(t, "{}", msg).unwrap();
+            }
+        }
+        t.reset().unwrap();
+    }
+}
+
+async fn _make_urls(fname: &str) -> Result<Urls, Box<dyn std::error::Error>> {
     let mut urls: Urls = Vec::new();
 
-    let f = File::open(fname).unwrap();
+    let f = File::open(fname).with_context(|| format!("Couldn't open '{}'", fname))?;
     let reader = BufReader::new(f);
 
     for line_ in reader.lines() {
         let line = line_.unwrap().to_lowercase();
+
+        // skip blank lines
         if line.is_empty() {
             continue;
         }
-        if line.chars().nth(0).unwrap() == '#' {
+
+        // skip lines without a URL
+        if line.chars().nth(0).unwrap() != 'h' {
             continue;
         }
+
         let link = Url::parse(&line)?;
         match link.scheme() {
             "http" | "https" => urls.push(link.to_string()),
@@ -38,7 +93,7 @@ async fn _check_urls(urls: Vec<String>) -> Result<CheckedUrls, Box<dyn std::erro
         let resp = reqwest::get(link).await;
         let status = match resp {
             Ok(res) => res.status().as_u16(),
-            Err(_) => 999 as u16,
+            Err(_) => 522 as u16, // Use CloudFlare's error status code
         };
         result.insert(link.to_string(), status);
     }
@@ -49,37 +104,10 @@ pub async fn check_urls(url_file: &str) -> Result<(), Box<dyn std::error::Error>
     let urls = _make_urls(url_file).await?;
     let results = _check_urls(urls).await?;
 
-    let mut t = term::stdout().unwrap();
     for (link, status) in results {
-        match status {
-            200..=299 => {
-                t.fg(term::color::GREEN).unwrap();
-                write!(t, "◉ ").unwrap();
-                t.fg(term::color::WHITE).unwrap();
-                writeln!(t, "{}", link).unwrap();
-            }
-            400..=599 => {
-                t.fg(term::color::YELLOW).unwrap();
-                write!(t, "◉ ").unwrap();
-                t.fg(term::color::WHITE).unwrap();
-                writeln!(t, "{}", link).unwrap();
-            }
-            999 => {
-                t.fg(term::color::RED).unwrap();
-                write!(t, "◉ ").unwrap();
-                t.fg(term::color::WHITE).unwrap();
-                writeln!(t, "{} - Error: couldn't connect", link).unwrap();
-            }
-            _ => {
-                t.fg(term::color::RED).unwrap();
-                write!(t, "◉ ").unwrap();
-                t.fg(term::color::WHITE).unwrap();
-                writeln!(t, "{}", link).unwrap();
-            }
-        }
+        let res = Status::new(status, &link);
+        res.print();
     }
-
-    t.reset().unwrap();
 
     Ok(())
 }
